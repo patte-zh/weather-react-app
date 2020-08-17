344292c0cdca8a81f194a7f4bf11b7506cc9c4d3
patte-zh <64026251+patte-zh@users.noreply.github.com>
2020-08-17 19:22:34 +0100

    try to fix the netlify deploys through github link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,12 @@ import "./footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <a href="https://github.com/patte-zh/weather-react-app" id="openSource">
+      <a
+        href="https://github.com/patte-zh/weather-react-app"
+        id="openSource"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Open-source code
       </a>
       <span> by patricia rodrigues</span>
